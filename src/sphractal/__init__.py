f655fef441bf9c4ff426a35b3db6c65df89862d5
Jon-Ting <jonting97@gmail.com>
2023-06-29 01:18:08 +1000

    feat(init): added imports for more functions

--- src/sphractal/__init__.py
+++ src/sphractal/__init__.py
@@ -3,6 +3,8 @@ from importlib.metadata import version
 __version__ = version('sphractal')
 
 # Populate package namespace
-__all__ = ['constants', 'boxCnt']
+__all__ = ['constants', 'datasets', 'utils', 'boxCnt']
 from sphractal.constants import ATOMIC_RAD_DICT, METALLIC_RAD_DICT
-from sphractal.boxCnt import runBoxCnt
+from sphractal.datasets import getExampleDataPath
+from sphractal.utils import readXYZ, findNN, findSurf
+from sphractal.boxCnt import getVoxelBoxCnts, getSphereBoxCnts, findSlope, runBoxCnt
